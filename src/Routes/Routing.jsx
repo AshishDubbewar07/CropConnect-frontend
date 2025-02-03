1c0e385bbe6f63b5752193355ed2d063b2600e9a
devashish1910 <devashishjaybhaye@gmail.com>
2025-02-03 12:03:36 +0530

    Renamed Routing.jsx with correct case

--- src/Routes/Routing.jsx
+++ src/Routes/Routing.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { createBrowserRouter } from "react-router";
-import Login from "../Components/common/Login";
-import Register from "../Components/common/Register";
-import About from "../Components/common/About";
-import ContactUs from "../Components/common/ContactUs";
-import Home from "../Components/common/Home";
+import Login from "../Components/Login";
+import Register from "../Components/Register";
+// import About from "../Components/About";
+// import ContactUs from "../Components/ContactUs";
+import Home from "../pages/Home";
 
 
 const router=createBrowserRouter([
     {path:"", element:<Home/>},
     {path:"/login",element:<Login/>},
     {path:"/register", element:<Register/>},
-    {path:"/about", element:<About/>},
-    {path:"/contact", element:<ContactUs/>}
+    // {path:"/about", element:<About/>},
+    // {path:"/contact", element:<ContactUs/>}
 
 ])
 
